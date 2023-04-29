e6653b361c65828eac81f4c603fdad8667b88e68
MH <729263080@qq.com>
2023-04-29 23:16:03 +0800

    Initial Commit

---
+++ 抽象工厂模式/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  抽象工厂模式
+//
+//  Created by menghao on 2023/4/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
